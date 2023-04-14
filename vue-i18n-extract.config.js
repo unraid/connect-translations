5161994b1a6481df0edba0f4a2da3cafbe6764cc
Eli Bosley <ekbosley@gmail.com>
2023-04-14 11:01:51 -0400

    feat: finalize unification

---
+++ vue-i18n-extract.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "vueFiles": "../connect/src/**/*.?(js|vue|ts)",
+  "languageFiles": "translations/*.?(json|yml|yaml|js)",
+  "exclude": [],
+  "report": true,
+  "output": false,
+  "add": true,
+  "remove": true,
+  "ci": false,
+  "separator": "BLAHBLAHBLAHNOSEPARATORPLEASE",
+  "noEmptyTranslation": "",
+  "missingTranslationString": null
+}
